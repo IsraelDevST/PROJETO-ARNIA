39bb0af45c3f36781c6d6aa8a280fa3a0ef5b283
IsraelDevST <124109844+IsraelDevST@users.noreply.github.com>
2023-05-09 23:08:37 -0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -50,37 +50,38 @@ btnSubmit2.addEventListener("click", function() {
     });
 });
 function minhaFuncao() {
-    // Obter os valores de email e senha do formulário de login
-    const email = document.getElementById("email").value;
-    const senha = document.getElementById("senha").value;
-  
-    const dadosLogin = {
-      email,
-      senha
-    };
-  
-    // Enviar os dados de login para a API
-    fetch("https://psiwexer-nxqq.onrender.com/usuarios", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(dadosLogin),
+  // Obter os valores de email e senha do formulário de login
+  const email = document.getElementById("email").value;
+  const senha = document.getElementById("senha").value;
+
+  const dadosLogin = {
+    email,
+    senha
+  };
+
+  // Enviar os dados de login para a API
+  fetch("https://psiwexer-nxqq.onrender.com/usuarios", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(dadosLogin),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      // Se os dados estiverem corretos, redirecionar o usuário para a página desejada
+      if (data.length > 0 && data[0].password[0] === senha) {
+        window.location = "./inicio.html"
+      } else {
+        // Caso contrário, exibir uma mensagem de erro
+        alert("Email ou senha incorretos.");
+      }
     })
-      .then((response) => response.json())
-      .then((data) => {
-        // Se os dados estiverem corretos, redirecionar o usuário para a página desejada
-        if (data.autenticado) {
-          window.location = "./inicio.html"
-        } else {
-          // Caso contrário, exibir uma mensagem de erro
-          alert("Email ou senha incorretos.");
-        }
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  }
+    .catch((error) => {
+      console.error(error);
+    });
+}
+
   
 function openModal() {
   const modal = document.getElementById("modalsair");
